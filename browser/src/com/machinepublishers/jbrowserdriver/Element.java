78f1ea7ce30554e7697426030ae5420bb60666d7
hollingsworthd <dan@machinepublishers.com>
2015-07-01 11:06:33 -0400

    Clarify license

--- browser/src/com/machinepublishers/jbrowserdriver/Element.java
+++ browser/src/com/machinepublishers/jbrowserdriver/Element.java
@@ -7,21 +7,6 @@
  * You can redistribute this program and/or modify it under the terms of the GNU Affero General Public
  * License version 3 as published by the Free Software Foundation.
  *
- * jBrowserDriver is made available under the terms of the GNU Affero General Public License version 3
- * with the following clarification and special exception:
- *
- *   Linking jBrowserDriver statically or dynamically with other modules is making a combined work
- *   based on jBrowserDriver. Thus, the terms and conditions of the GNU Affero General Public License
- *   version 3 cover the whole combination.
- *
- *   As a special exception, Machine Publishers, LLC gives you permission to link unmodified versions
- *   of jBrowserDriver with independent modules to produce an executable, regardless of the license
- *   terms of these independent modules, and to copy, distribute, and make available the resulting
- *   executable under terms of your choice, provided that you also meet, for each linked independent
- *   module, the terms and conditions of the license of that module. An independent module is a module
- *   which is not derived from or based on jBrowserDriver. If you modify jBrowserDriver, you may not
- *   extend this exception to your modified version of jBrowserDriver.
- *
  * "ScreenSlicer", "jBrowserDriver", "Machine Publishers", and "automatic, zero-config web scraping"
  * are trademarks of Machine Publishers, LLC.
  * 
@@ -30,10 +15,10 @@
  * Affero General Public License version 3 for more details.
  * 
  * You should have received a copy of the GNU Affero General Public License version 3 along with this
- * program. If not, see <http://www.gnu.org/licenses/>.
+ * program. If not, see http://www.gnu.org/licenses/
  * 
- * For general details about how to investigate and report license violations, please see:
- * <https://www.gnu.org/licenses/gpl-violation.html> and email the author: ops@machinepublishers.com
+ * For general details about how to investigate and report license violations, please see
+ * https://www.gnu.org/licenses/gpl-violation.html and email the author, ops@machinepublishers.com
  */
 package com.machinepublishers.jbrowserdriver;
 
