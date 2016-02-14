686461a16acedc6e5e797ebe244ae5485d748ee1
hollingsworthd <dan@machinepublishers.com>
2016-02-13 20:26:49 -0500

    Use SecurityManager class instead of policy file. Fixes #38.

--- src/com/machinepublishers/jbrowserdriver/Policy.java
+++ src/com/machinepublishers/jbrowserdriver/Policy.java
@@ -19,21 +19,113 @@
  */
 package com.machinepublishers.jbrowserdriver;
 
-import java.io.File;
-import java.nio.file.Files;
+import java.io.FileDescriptor;
+import java.net.InetAddress;
+import java.security.Permission;
 
-class Policy {
+class Policy extends SecurityManager {
   static void init() {
     if (System.getSecurityManager() == null) {
       try {
-        File policy = File.createTempFile("jbd_policy_", ".config");
-        policy.deleteOnExit();
-        Files.write(policy.toPath(), "grant{permission java.security.AllPermission;};".getBytes("utf-8"));
-        System.setProperty("java.security.policy", policy.getAbsolutePath());
-        System.setSecurityManager(new SecurityManager());
+        System.setSecurityManager(new Policy());
       } catch (Throwable t) {
         Logs.fatal(t);
       }
     }
   }
+
+  @Override
+  public void checkPermission(Permission perm) {}
+
+  @Override
+  public void checkPermission(Permission perm, Object context) {}
+
+  @Override
+  public void checkCreateClassLoader() {}
+
+  @Override
+  public void checkAccess(Thread t) {}
+
+  @Override
+  public void checkAccess(ThreadGroup g) {}
+
+  @Override
+  public void checkExit(int status) {}
+
+  @Override
+  public void checkExec(String cmd) {}
+
+  @Override
+  public void checkLink(String lib) {}
+
+  @Override
+  public void checkRead(FileDescriptor fd) {}
+
+  @Override
+  public void checkRead(String file) {}
+
+  @Override
+  public void checkRead(String file, Object context) {}
+
+  @Override
+  public void checkWrite(FileDescriptor fd) {}
+
+  @Override
+  public void checkWrite(String file) {}
+
+  @Override
+  public void checkDelete(String file) {}
+
+  @Override
+  public void checkConnect(String host, int port) {}
+
+  @Override
+  public void checkConnect(String host, int port, Object context) {}
+
+  @Override
+  public void checkListen(int port) {}
+
+  @Override
+  public void checkAccept(String host, int port) {}
+
+  @Override
+  public void checkMulticast(InetAddress maddr) {}
+
+  @Override
+  public void checkMulticast(InetAddress maddr, byte ttl) {}
+
+  @Override
+  public void checkPropertiesAccess() {}
+
+  @Override
+  public void checkPropertyAccess(String key) {}
+
+  @Override
+  public boolean checkTopLevelWindow(Object window) {
+    return true;
+  }
+
+  @Override
+  public void checkPrintJobAccess() {}
+
+  @Override
+  public void checkSystemClipboardAccess() {}
+
+  @Override
+  public void checkAwtEventQueueAccess() {}
+
+  @Override
+  public void checkPackageAccess(String pkg) {}
+
+  @Override
+  public void checkPackageDefinition(String pkg) {}
+
+  @Override
+  public void checkSetFactory() {}
+
+  @Override
+  public void checkMemberAccess(Class<?> clazz, int which) {}
+
+  @Override
+  public void checkSecurityAccess(String target) {}
 }
