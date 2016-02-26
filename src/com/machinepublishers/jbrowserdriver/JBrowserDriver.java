a2e21ebfd0851bb389764d449b85c0d8fa4e5d96
hollingsworthd <dan@machinepublishers.com>
2016-02-26 04:48:26 -0500

    Make a copy of array parameter so original is not mutated.

--- src/com/machinepublishers/jbrowserdriver/JBrowserDriver.java
+++ src/com/machinepublishers/jbrowserdriver/JBrowserDriver.java
@@ -729,8 +729,7 @@ public class JBrowserDriver extends RemoteWebDriver implements Killable {
   @Override
   public Object executeAsyncScript(String script, Object... args) {
     try {
-      prepareScriptParams(args);
-      return Element.constructObject(remote.executeAsyncScript(script, args), this, logs);
+      return Element.constructObject(remote.executeAsyncScript(script, scriptParams(args)), this, logs);
     } catch (RemoteException e) {
       logs.exception(e);
       return null;
@@ -743,22 +742,28 @@ public class JBrowserDriver extends RemoteWebDriver implements Killable {
   @Override
   public Object executeScript(String script, Object... args) {
     try {
-      prepareScriptParams(args);
-      return Element.constructObject(remote.executeScript(script, args), this, logs);
+      return Element.constructObject(remote.executeScript(script, scriptParams(args)), this, logs);
     } catch (RemoteException e) {
       logs.exception(e);
       return null;
     }
   }
 
-  private static void prepareScriptParams(Object[] args) {
-    for (int i = 0; args != null && i < args.length; i++) {
-      if (args[i] instanceof Element) {
-        ElementId id = new ElementId();
-        ((Element) args[i]).scriptParam(id);
-        args[i] = id;
+  private static Object[] scriptParams(Object[] args) {
+    if (args != null) {
+      Object[] argsOut = new Object[args.length];
+      for (int i = 0; i < args.length; i++) {
+        if (args[i] instanceof Element) {
+          ElementId id = new ElementId();
+          ((Element) args[i]).scriptParam(id);
+          argsOut[i] = id;
+        } else {
+          argsOut[i] = args[i];
+        }
       }
+      return argsOut;
     }
+    return null;
   }
 
   /**
